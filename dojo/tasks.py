4dadcf43d341dd684e2a5df5c8f946cd67fb3e31
PatrickMayo <pmayo@blacksky.com>
2020-11-09 12:32:50 -0500

    add out of scope and mitigated to celery task check

--- dojo/tasks.py
+++ dojo/tasks.py
@@ -275,7 +275,7 @@ def latest_findings_only(self, username, age):
                 if finding not in eng_findings:
                     eng_findings.append(finding)
         for finding in eng_findings:
-            if (not finding.active or not finding.verified) and not finding.false_p:
+            if (not finding.active or not finding.verified) and (not finding.false_p or not finding.out_of_scope or not finding.mitigated):
                 finding.active = True
                 finding.verified = True
                 finding.save()
